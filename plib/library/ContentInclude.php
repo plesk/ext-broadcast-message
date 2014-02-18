7df496ed07134a711d7d12a93c680b30c30ef24a
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-02-18 18:30:20 +0700

    Initial draft of broadcast message extension.

---
+++ plib/library/ContentInclude.php
@@ -0,0 +1,33 @@
+<?php
+
+class Modules_BroadcastMessage_ContentInclude
+{
+
+    public function init()
+    {
+        info('test message');
+    }
+
+    public function getJsConfig()
+    {
+        return [
+            'plesk-version' => '12.0',
+        ];
+    }
+
+    public function getJsOnReadyContent()
+    {
+        return 'PleskExt.BroadcastMessage.init();';
+    }
+
+    public function getJsContent()
+    {
+        return '// example';
+    }
+
+    public function getHtmlContent()
+    {
+        return '<!-- xx -->';
+    }
+
+}
