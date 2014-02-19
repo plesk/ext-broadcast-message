21fb4637cfb19471a6dfd70084fda4420e963f01
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-02-19 15:40:12 +0700

    Show message only if user is logged in.

--- plib/library/ContentInclude.php
+++ plib/library/ContentInclude.php
@@ -3,29 +3,31 @@
 class Modules_BroadcastMessage_ContentInclude
 {
 
-    public function init()
+    public static function init()
     {
-        pm_View_Status::addInfo('info message');
+        if (pm_Session::isExist()) {
+            pm_View_Status::addInfo('info message');
+        }
     }
 
-    public function getJsConfig()
+    public static function getJsConfig()
     {
         return [
             'plesk-version' => '12.0',
         ];
     }
 
-    public function getJsOnReadyContent()
+    public static function getJsOnReadyContent()
     {
         return 'PleskExt.BroadcastMessage.init();';
     }
 
-    public function getJsContent()
+    public static function getJsContent()
     {
         return '// example';
     }
 
-    public function getHeadContent()
+    public static function getHeadContent()
     {
         return '<!-- some content inside head tag -->';
     }
