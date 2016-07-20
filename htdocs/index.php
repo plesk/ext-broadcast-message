87daa0598eea15ecf2513aa0d1bc920edfc55da4
Alexei Yuzhakov <ayuzhakov@plesk.com>
2016-07-20 17:18:04 +0600

    Update copyright year.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2015. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
 pm_Context::init('broadcast-message');
 
 $application = new pm_Application();
